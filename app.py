6fa6f17dd91957c1ac291e4a0d67f13aa2724e06
Alan Y Han <ayh9@cornell.edu>
2024-05-14 00:31:34 -0400

    Add imports and df

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template, jsonify
 
 import os 
 
+import matplotlib.pyplot as plt
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator  
+import numpy as np 
+import pandas as pd
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
@@ -28,7 +34,8 @@ def upload_file ():
   
   return render_template ('upload.html')
   
-
+df = pd.read_csv('data/food.csv')
+print(df.head())
 
 if __name__ == '__main__':
   app.run (debug = True )
